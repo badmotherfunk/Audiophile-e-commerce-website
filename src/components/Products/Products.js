3f29fc4a4ac8f87fabd3f4c3782c049a1b53ca08
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-02 19:22:39 +0100

    Implémentation du composant 'Products' et du responsive pour la résolution tablet

---
+++ src/components/Products/Products.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './products.scss'
+
+export default function Products() {
+  return (
+    <section className='section-products'>
+        
+        <div className='products'>
+            <Link className="products__layout" to={'/headphones'}>
+
+                <picture className='products__picture'>
+                    <img src="/starter-code/assets/shared/desktop/image-category-thumbnail-headphones.png" alt="" />
+                </picture>
+
+                <div className="products__content">
+                    <p className='products__title'>HEADPHONES</p>
+                    <div className="products__shop">
+                        <p>SHOP</p>
+                        <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+                    </div>
+                </div>
+                
+            </Link>
+            <Link className="products__layout" to={'/speakers'}>
+
+                <picture className='products__picture'>
+                    <img src="/starter-code/assets/shared/desktop/image-category-thumbnail-speakers.png" alt="" />
+                </picture>
+
+                <div className="products__content">
+                    <p className='products__title'>SPEAKERS</p>
+                    <div className="products__shop">
+                        <p>SHOP</p>
+                        <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+                    </div>
+                </div>
+                
+            </Link>
+
+            <Link className="products__layout" to={'/earphones'}>
+
+                <picture className='products__picture'>
+                    <img src="/starter-code/assets/shared/desktop/image-category-thumbnail-earphones.png" alt="" />
+                </picture>
+
+                <div className="products__content">
+                    <p className='products__title'>EARPHONES</p>
+                    <div className="products__shop">
+                        <p>SHOP</p>
+                        <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+                    </div>
+                </div>
+                
+            </Link>
+
+        </div>
+
+    </section>
+  )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import './home.scss'
 import ButtonFilled from '../../components/ButtonFilled/ButtonFilled'
+import Products from '../../components/Products/Products'
 
 export default function Home() {
   return (
-    <header className='home-header'>
+    <>
+      <header className='home-header'>
 
-      <div className="hero-container">
-        <div className="hero-content">
-          <p className='hero-title'>NEW PRODUCT</p>
-          <h1 className='hero-product'>XX99 MARK II HEADPHONES</h1>
-          <p className='hero-description'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusisast.</p>
-          <ButtonFilled />
+        <div className="hero-container">
+          <div className="hero-content">
+            <p className='hero-title'>NEW PRODUCT</p>
+            <h1 className='hero-product'>XX99 MARK II HEADPHONES</h1>
+            <p className='hero-description'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusisast.</p>
+            <ButtonFilled />
+          </div>
         </div>
-      </div>
 
-      <picture>
-        <source media="(max-width: 480px)" srcset={'/starter-code/assets/home/mobile/image-header.jpg'} />
-        <source media="(max-width: 1024px)" srcset={'/starter-code/assets/home/tablet/image-header.jpg'} />
-        <img src='/starter-code/assets/home/desktop/image-hero.jpg' alt="mark-two headphones" />
-      </picture>
+        <picture className='hero-picture'>
+          <source media="(max-width: 480px)" srcset={'/starter-code/assets/home/mobile/image-header.jpg'} />
+          <source media="(max-width: 1024px)" srcset={'/starter-code/assets/home/tablet/image-header.jpg'} />
+          <img src='/starter-code/assets/home/desktop/image-hero.jpg' alt="mark-two headphones" />
+        </picture>
 
-    </header>
+      </header>
+
+      <main>
+          <Products />
+      </main>
+    </>
   )
 }
