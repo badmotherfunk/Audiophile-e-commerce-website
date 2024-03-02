f7ee18fc2298acfb8560c9b4801efb29e31f67f6
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-02 10:00:41 +0100

    Implémentation du header et des breakpoints pour le responsive

---
+++ src/components/ButtonFilled/ButtonFilled.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./buttonFilled.scss"
+
+export default function ButtonFilled() {
+  return (
+    <button className='buttonFilled'>
+        SEE PRODUCT
+    </button>
+  )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './home.scss'
+import ButtonFilled from '../../components/ButtonFilled/ButtonFilled'
 
 export default function Home() {
   return (
-    <div className='home'>
-      Home
-    </div>
+    <header className='home-header'>
+      <img src='/starter-code/assets/home/desktop/image-hero.jpg' alt="mark-two headphones" />
+      <div className="hero-container">
+        <div className="hero-content">
+          <p className='hero-title'>NEW PRODUCT</p>
+          <h1 className='hero-product'>XX99 MARK II HEADPHONES</h1>
+          <p className='hero-description'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusisast.</p>
+          <ButtonFilled />
+        </div>
+      </div>
+    </header>
   )
 }
