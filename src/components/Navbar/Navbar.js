ea8be8b51af824924cde0e542616fc8990d745fa
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-03 13:07:24 +0100

    Amélioration du comportement du menu de navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.scss'
 import Products from '../Products/Products'
@@ -13,6 +12,19 @@ export default function Navbar() {
     setActive(!active)
   }
 
+  let modalRef = useRef();
+
+  function checkClickOutside(e) {
+    if(active && !modalRef.current.contains(e.target || e.target.className === "navbar-products active")) {
+      setActive(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', checkClickOutside)
+  })
+
+
   return (
     <div className='navbar' >
 
@@ -23,7 +35,7 @@ export default function Navbar() {
         </div>
 
         <div className={active ? "navbar__close active" : "navbar__close"} onClick={toggleActive}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="#FFF" viewBox="0 0 30 30">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFF" viewBox="0 0 30 30">
           <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
           </svg>
         </div>
@@ -54,7 +66,7 @@ export default function Navbar() {
       </div>
 
       <div className={active ? "navbar-overlay active" : "navbar-overlay"}></div>
-      <div className={active ? "navbar-products active" : "navbar-products"} onClick={toggleActive}>
+      <div ref={modalRef} className={active ? "navbar-products active" : "navbar-products"}  onClick={toggleActive}>
         <Products />
       </div>
 
