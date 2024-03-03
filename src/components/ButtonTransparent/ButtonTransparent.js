dd81ce13dd6730b0fb7e03b0ac3eafbbad73e02d
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-03 18:30:40 +0100

    Implémentation et gestion du responsive pour le main product de la page d'accueil

---
+++ src/components/ButtonTransparent/ButtonTransparent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './buttonTransparent.scss'
+
+export default function ButtonTransparent() {
+  return (
+    <button className='buttonTransparent'>
+        SEE PRODUCT
+    </button>
+  )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './home.scss'
-import ButtonFilled from '../../components/ButtonFilled/ButtonFilled'
-import Products from '../../components/Products/Products'
+import ButtonFilled from '../../components/ButtonFilled/ButtonFilled';
+// import ButtonTransparent from '../../components/ButtonTransparent/ButtonTransparent';
+import Products from '../../components/Products/Products';
 
 export default function Home() {
   return (
@@ -27,6 +28,35 @@ export default function Home() {
 
       <main>
           <Products />
+
+          <section className='products-section'>
+            <div className="main-product">
+
+              <svg className='main-product-svg' width="944" height="944" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" fill="none" fill-rule="evenodd" opacity=".202"><circle cx="472" cy="472" r="235.5"/><circle cx="472" cy="472" r="270.5"/><circle cx="472" cy="472" r="471.5"/></g></svg>
+
+              <div className="products-main-container">
+                <picture className='product-main-picture'>
+                  <source media="(max-width: 480px)" srcset={'/starter-code/assets/home/mobile/image-speaker-zx9.png'} />
+                  <source media="(max-width: 1024px)" srcset={'/starter-code/assets/home/tablet/image-speaker-zx9.png'} />
+                  <img src="/starter-code/assets/home/desktop/image-speaker-zx9.png" alt="speaker-zx9" />
+                </picture>
+                  
+                  <div className="product-main-content">
+                    <div className="product-content">
+
+                      <p className='product-title-main'>ZX9 SPEAKER</p>
+                      <p className='product-description-main'>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                      <button className='button-speaker-main'>
+                        SEE PRODUCT
+                      </button>
+                    </div>
+
+                  </div>
+
+              </div>
+
+            </div>
+          </section>
       </main>
     </>
   )
