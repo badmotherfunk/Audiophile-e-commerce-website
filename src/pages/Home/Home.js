582a57825471b4c6d3214de9b4ce11d7d7cb5654
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-04 11:32:13 +0100

    Implémentation du second produit de la main page et du responsive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './home.scss'
 import ButtonFilled from '../../components/ButtonFilled/ButtonFilled';
-// import ButtonTransparent from '../../components/ButtonTransparent/ButtonTransparent';
+import ButtonTransparent from '../../components/ButtonTransparent/ButtonTransparent';
 import Products from '../../components/Products/Products';
 
 export default function Home() {
@@ -32,30 +32,38 @@ export default function Home() {
           <section className='products-section'>
             <div className="main-product">
 
-              <svg className='main-product-svg' width="944" height="944" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" fill="none" fill-rule="evenodd" opacity=".202"><circle cx="472" cy="472" r="235.5"/><circle cx="472" cy="472" r="270.5"/><circle cx="472" cy="472" r="471.5"/></g></svg>
-
-              <div className="products-main-container">
+              <div className="product-main-container">
                 <picture className='product-main-picture'>
                   <source media="(max-width: 480px)" srcset={'/starter-code/assets/home/mobile/image-speaker-zx9.png'} />
                   <source media="(max-width: 1024px)" srcset={'/starter-code/assets/home/tablet/image-speaker-zx9.png'} />
                   <img src="/starter-code/assets/home/desktop/image-speaker-zx9.png" alt="speaker-zx9" />
                 </picture>
                   
-                  <div className="product-main-content">
-                    <div className="product-content">
+                <div className="product-main-content">
+                  <div className="product-content">
 
-                      <p className='product-title-main'>ZX9 SPEAKER</p>
-                      <p className='product-description-main'>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                      <button className='button-speaker-main'>
-                        SEE PRODUCT
-                      </button>
-                    </div>
-
+                    <p className='product-title-main'>ZX9 SPEAKER</p>
+                    <p className='product-description-main'>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                    <button className='button-speaker-main'>
+                      SEE PRODUCT
+                    </button>
                   </div>
+                </div>
 
               </div>
 
             </div>
+
+            <div className="secondary-product">
+              <div className="secondary-product-container">
+                
+                <div className="secondary-product-content">
+                  <p className='product-title-secondary'>ZX7 SPEAKER</p>
+                    <ButtonTransparent />
+                </div>
+              </div>
+            </div>
+            
           </section>
       </main>
     </>
