c0eaabc65997ce7ff3bc4f2981fc9e3c848b2830
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-03-17 12:10:44 +0100

    Restauration de la validation des inputs au 'onBlur'

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -200,6 +200,7 @@ export default function Checkout() {
     }
 
     const handleValidation = () => {
+        setError(validate(formValues))
         setErrorValidation(validation(formValues))
     }
 
